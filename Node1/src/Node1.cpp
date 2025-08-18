11e22502bda31313b05b36e630c8e02461f0b318
WonSeok-Song <117966644+wonseokkkk@users.noreply.github.com>
2025-08-18 15:33:43 +0900

    Update Node1.cpp

--- Node1/src/Node1.cpp
+++ Node1/src/Node1.cpp
@@ -25,7 +25,7 @@ uint64_t measureLatency(F&& func) {
     auto t0 = std::chrono::high_resolution_clock::now();
     func();
     auto t1 = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+    return std::chrono::duration_cast<std::chrono::miliseconds>(t1 - t0).count();
 }
 
 // 2) 유틸 함수
